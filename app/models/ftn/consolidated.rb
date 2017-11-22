0e42674fb6eb8bac9561962b25158fef2d2c9cff
noreplyror <noreplyror@gmail.com>
2017-11-22 19:09:02 +0530

    changes for non-us ftn

---
+++ app/models/ftn/consolidated.rb
@@ -0,0 +1,49 @@
+module Ftn
+  class Consolidated
+    include Searchable
+    self.model_classes = [Ftn::Rpl]
+    self.fetch_all_sort_by = 'name.keyword'
+
+    include SeparatedValuesable
+    self.separated_values_config = [
+      { source: [:full_name] },
+      :entity_number,
+      :type,
+      :programs,
+      :name,
+      :title,
+      :address_one_desc,
+      :address_two_desc
+      :federal_register_notice,
+      :start_date,
+      :end_date,
+      :standard_order,
+      :license_requirement,
+      :license_policy,
+      :call_sign,
+      :vessel_type,
+      :gross_tonnage,
+      :gross_registered_tonnage,
+      :vessel_flag,
+      :vessel_owner,
+      :remarks,
+      :source_list_url,
+      :alt_names,
+      :citizenships,
+      :dates_of_birth,
+      :nationalities,
+      :places_of_birth,
+      :source_information_url,
+      { ids: [:country, :expiration_date, :issue_date, :number, :type] }
+    ]
+
+    def self.search_for(options)
+      result = super(options)
+      if options['fuzzy_name'].present? && options['name'].present?
+        score_adjuster = ScoreAdjuster.new(options['name'], result[:hits])
+        result[:hits] = score_adjuster.rescored_hits
+      end
+      result
+    end
+  end
+end
---
+++ app/models/ftn/mappable.rb
@@ -0,0 +1,77 @@
+module Ftn
+  module Mappable
+    def self.included(klass)
+      binding.pry
+      klass.import_rate = 'Hourly'
+      klass.analyze_by :snowball_asciifolding_nostop, :standard_asciifolding_nostop, :keyword_asciifolding_lowercase, :keyword_asciifolding_uppercase
+
+      klass.settings.freeze
+      klass.mappings = {
+        klass.name.typeize => {
+          properties: {
+            _updated_at:                { type: 'date', format: 'strictDateOptionalTime' },
+            # base names
+            address_one_desc:           { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            address_two_desc:           { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            aka_name_nm:                { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword', analyzer: 'keyword_asciifolding_lowercase' }, }, },
+            city_nm:                    { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            contact_nm:                 { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            country_nm:                 { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            customs_area_cd:            { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            effective_dt:               { type: 'date' },
+            
+            expiration_dt:              { type: 'date' },
+            
+            ignored_party_flag:         { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            last_modification_tmstp:    { type: 'date' },
+            
+            name_no_ws_rev_with_common: { type: 'text', analyzer: 'standard_asciifolding_nostop',
+                                          term_vector: 'with_positions_offsets', },
+            name_nm:                    { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            oid_nbr:                    { type: 'long' },
+
+            source_agency_nm:           { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            source_country_nm:          { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            source_list_nm:             { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            source_url_nm:              { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            state_province_nm:          { type:        'text',
+                                          fields:      {
+                                            keyword: { type: 'keyword' }, }, },
+            },
+        },
+      }.merge(klass.metadata_mappings,).freeze
+
+      klass.class_eval do
+        class << self
+          attr_accessor :source
+        end
+      end
+    end
+  end
+end
